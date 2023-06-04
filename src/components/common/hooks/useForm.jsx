a4f40ed30ec57d6f855271ae5d5a67ad67d18d9a
hsejsx <hsejsx@gmail.com>
2023-06-05 08:25:08 +0900

    Feat: Form 커스텀 훅 설정

---
+++ src/components/common/hooks/useForm.jsx
@@ -0,0 +1,35 @@
+import { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+
+export default function useForm() {
+  const navigate = useNavigate();
+  const [form, setForm] = useState({
+    email: '',
+    password: '',
+    isActive: false,
+  });
+
+  const handleChange = e => {
+    const { name, value } = e.target;
+    setForm(prev => ({ ...prev, [name]: value }));
+  };
+
+  useEffect(() => {
+    localStorage.getItem('token') && navigate('/todo');
+  }, [navigate]);
+
+  useEffect(() => {
+    const pattern = /^[^@].*[^@]$/;
+    if (pattern.test(form.email) && form.password.length >= 8) {
+      if (!form.isActive) {
+        setForm(prev => ({ ...prev, isActive: true }));
+      }
+    } else {
+      if (form.isActive) {
+        setForm(prev => ({ ...prev, isActive: false }));
+      }
+    }
+  }, [form]);
+
+  return [form, handleChange];
+}
