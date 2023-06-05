e444cd3d2bad74c7714afbc42256d5345257eb64
hsejsx <hsejsx@gmail.com>
2023-06-05 09:26:16 +0900

    Feat: Todo 리다이렉트

--- src/pages/Todo.jsx
+++ src/pages/Todo.jsx
@@ -1,15 +1,21 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import Header from '../components/todo/Header';
 import TodoList from '../components/todo/TodoList';
 import TodoForm from '../components/todo/TodoForm';
 import Sidebar from '../components/todo/Sidebar';
+import { useNavigate } from 'react-router-dom';
 
 export default function Todo() {
   const [isOpen, setIsOpen] = useState(false);
+  const navigate = useNavigate();
 
   const handleOpen = () => {
     setIsOpen(prev => !prev);
   };
+
+  useEffect(() => {
+    !localStorage.getItem('token') && navigate('/signin');
+  }, [navigate]);
 
   return (
     <section className='w-full h-screen'>
