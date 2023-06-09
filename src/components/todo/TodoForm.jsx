6877acd78674b96e4131212428965b8a6cd2508a
hsejsx <hsejsx@gmail.com>
2023-06-09 10:14:05 +0900

    Feat: TodoForm 설정

--- src/components/todo/TodoForm.jsx
+++ src/components/todo/TodoForm.jsx
@@ -1,14 +1,45 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { createTodoApi } from '../../api/todos';
 
-export default function TodoForm() {
+export default function TodoForm({ setTodos }) {
+  const [newTodo, setNewTodo] = useState('');
+
+  const handleChange = e => {
+    setNewTodo(e.target.value);
+  };
+
+  const handleSubmit = async e => {
+    e.preventDefault();
+    const body = {
+      todo: newTodo,
+    };
+
+    try {
+      const res = await createTodoApi(body);
+      if (res.status !== 201) {
+        alert('할 일 추가에 실패했습니다.');
+        return;
+      }
+
+      setTodos(prev => [...prev, res.data]);
+    } catch (error) {
+      console.error(error);
+      alert('에러가 발생했습니다.');
+    } finally {
+      setNewTodo('');
+    }
+  };
+
   return (
-    <form className='fixed bottom-0 w-full'>
+    <form onSubmit={handleSubmit} className='fixed bottom-0 w-full'>
       <input
         data-testid='new-todo-input'
         className='w-full p-4 pr-16 text-xl outline-none'
         type='text'
         placeholder='할 일 추가하기'
         name='todo'
+        onChange={handleChange}
+        value={newTodo}
       />
       <button
         data-testid='new-todo-add-button'
