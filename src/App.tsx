db1d7cf9cc77e5225749a2df932c0a031b16249d
HyeonDoonge <migu554@naver.com>
2023-06-20 02:00:24 +0900

    로그인, Todo Client router 경로 추가 (#14)
    - /todo, /signin 라우트 경로 추가
    - #9

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import SignUpPage from 'components/SignUpPage/SignUpPage';
-import { SIGNUP_URL } from 'constants/route';
+import SignUpPage from 'components/SignUpPage';
+import SignInPage from 'components/SignInPage';
+import TodoPage from 'components/TodoPage';
+import { SIGNIN_URL, SIGNUP_URL, TODO_URL } from 'constants/route';
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path={SIGNUP_URL} element={<SignUpPage />} />
+        <Route path={SIGNIN_URL} element={<SignInPage />} />
+        <Route path={TODO_URL} element={<TodoPage />} />
       </Routes>
     </BrowserRouter>
   );
