bc1d9fd7e8456b4167814f20f8f062ad5ad8942a
Hyeondoonge <migu554@naver.com>
2023-06-30 02:37:41 +0900

    feat: Toast 전역 상태 관리 기능 추가

---
+++ src/ToastContext.jsx
@@ -0,0 +1,26 @@
+import React, { createContext, useMemo, useState } from 'react';
+import { createPortal } from 'react-dom';
+import Toast from './components/Toast';
+import ToastList from './components/ToastList';
+
+export const ToastContext = createContext(null);
+
+/* eslint-disable react/prop-types */
+export default function ToastsContextProvier({ children }) {
+  const [toasts, setToasts] = useState([]);
+  const data = useMemo(() => [toasts, setToasts], []);
+
+  return (
+    <ToastContext.Provider value={data}>
+      {children}
+      {createPortal(
+        <ToastList>
+          {toasts.map(({ message }) => (
+            <Toast>{message}</Toast>
+          ))}
+        </ToastList>,
+        document.getElementById('toast')
+      )}
+    </ToastContext.Provider>
+  );
+}
