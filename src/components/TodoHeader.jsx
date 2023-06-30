a3f4e13e70b09576be58e49ce4c4430363c21849
JYROH <ghgt1@naver.com>
2023-06-30 18:36:58 +0900

    feat: Header에 로그아웃 기능 추가

--- src/components/TodoHeader.jsx
+++ src/components/TodoHeader.jsx
@@ -1,12 +1,21 @@
 import React from 'react';
 import { styled } from 'styled-components';
 import { FaPowerOff } from 'react-icons/fa';
+import { useNavigate } from 'react-router-dom';
+import useToast from '../hooks/useToast';
 
 export default function TodoHeader() {
+  const navigate = useNavigate();
+  const createToast = useToast();
+  const handleLogout = () => {
+    localStorage.removeItem('access_token');
+    createToast({ message: '성공적으로 로그아웃 되었습니다.', type: 'success' });
+    navigate('/signin');
+  };
   return (
     <Header>
       <Title lang="en">ToDo List</Title>
-      <LogoutBtn type="LogoutBtn" aria-label="로그아웃">
+      <LogoutBtn type="LogoutBtn" aria-label="로그아웃" onClick={handleLogout}>
         <FaPowerOff />
       </LogoutBtn>
     </Header>
