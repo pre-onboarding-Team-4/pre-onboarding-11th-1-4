889412a55e7e815cdcae1be978140a30d5662d36
hsejsx <hsejsx@gmail.com>
2023-06-30 22:26:45 +0900

    design: auth 폼 CSS 수정

--- src/components/AuthForm.jsx
+++ src/components/AuthForm.jsx
@@ -105,18 +105,18 @@ const Title = styled.h2`
 `;
 
 const UserInputForm = styled.form`
-  width: 308px;
-  margin-bottom: 40px;
+  width: 100%;
+  max-width: 288px;
   display: flex;
   flex-direction: column;
   align-items: center;
-  gap: 30px;
 `;
 
 const EmailInput = styled.input`
-  padding: 0 5px;
+  padding: 0 12px;
   width: 288px;
   height: 48px;
+  margin-bottom: 8px;
   font-family: 'Noto Sans KR';
   font-style: normal;
   font-weight: 400;
@@ -124,12 +124,19 @@ const EmailInput = styled.input`
   line-height: 23px;
   color: placeholder;
   box-sizing: border-box;
+  outline: none;
+  border: 1px solid #6c757d;
+  border-radius: 4px;
+  &:focus {
+    border-color: var(--color-primary);
+  }
 `;
 
 const PwInput = styled.input`
-  padding: 0 5px;
+  padding: 0 12px;
   width: 288px;
   height: 48px;
+  margin-bottom: 8px;
   font-family: 'Noto Sans KR';
   font-style: normal;
   font-weight: 400;
@@ -137,27 +144,35 @@ const PwInput = styled.input`
   line-height: 23px;
   color: placeholder;
   box-sizing: border-box;
+  outline: none;
+  border: 1px solid #6c757d;
+  border-radius: 4px;
+  &:focus {
+    border-color: var(--color-primary);
+  }
 `;
 
 const EmailValidText = styled.p`
-  width: 170px;
-  height: 17px;
+  width: 100%;
+  padding-left: 12px;
+  margin: 0 0 8px;
+  text-align: left;
   font-family: 'Noto Sans KR';
   font-style: normal;
   font-weight: 350;
   font-size: 12px;
-  line-height: 17px;
   color: #fb0000;
 `;
 
 const PwValidText = styled.p`
-  width: 180px;
-  height: 17px;
+  width: 100%;
+  padding-left: 12px;
+  margin: 0 0 8px;
+  text-align: left;
   font-family: 'Noto Sans KR';
   font-style: normal;
   font-weight: 350;
   font-size: 12px;
-  line-height: 17px;
   color: #fb0000;
 `;
 
