56b19c062a4ab842e6b557e0523cf8c3573f6507
Hyeondoonge <migu554@naver.com>
2023-06-30 11:39:57 +0900

    feat: 컴포넌트 최상단에 ContextProvider 추가

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,12 @@
-import React, { useState } from 'react';
-import Toast from './Toast';
+import React from 'react';
+import ToastsContextProvier from './ToastContext';
+import Component from './Component';
 
 function App() {
-  const [toast, setToast] = useState({ message: '', index: 0 });
-
   return (
-    <div
-      className="App"
-      style={{
-        width: 500,
-        height: 300,
-        color: 'pink',
-        backgroundColor: 'black',
-        margin: '300px auto',
-        position: 'relative',
-      }}
-    >
-      <button
-        type="button"
-        onClick={() => {
-          setToast({ message: `${toast.index + 1}번 메시지`, index: toast.index + 1 });
-        }}
-      >
-        새로운 토스트 출력
-      </button>
-      <h1>토스트를 출력하려고합니다!</h1>
-      {toast.message && <Toast>{toast.message}</Toast>}
-    </div>
+    <ToastsContextProvier>
+      <Component />
+    </ToastsContextProvier>
   );
 }
 
